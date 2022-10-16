83d899f123ca3e58ad8fd5f4445f46482f89b27e
Fadhlan <fadhlan.hafiz@gmail.com>
2022-10-16 16:40:41 +0800

    first

---
+++ DL_mnist_predict.py
@@ -0,0 +1,32 @@
+# make a prediction for a new image.
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+import numpy as np
+
+# load and prepare the image
+def load_image(filename):
+	# load the image
+	img = load_img(filename, grayscale=True, target_size=(28, 28))
+	# convert to array
+	img = img_to_array(img)
+	# reshape into a single sample with 1 channel
+	img = img.reshape(1, 28, 28, 1)
+	# prepare pixel data
+	img = img.astype('float32')
+	img = img / 255.0
+	return img
+
+# load an image and predict the class
+def run_example():
+	# load the image
+    img = load_image('D:/Python Lesson/testSet/img_8.jpg')
+	# load model
+    model = load_model('final_model.h5')
+	# predict the class
+    digit = model.predict(img)
+    classes_x=np.argmax(digit,axis=1)
+    print(classes_x[0])
+
+# entry point, run the example
+run_example()
